63bbb2dad91d8fcff247eb7518f5eef25c7ea6e7
Khusharth <khusharth19@gmail.com>
2020-02-12 00:02:47 +0530

    Adding item to UI and Clear Fields after input

--- app.js
+++ app.js
@@ -69,6 +69,8 @@ var UIController = ( function() {
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list',
     };
 
     return {
@@ -81,6 +83,42 @@ var UIController = ( function() {
                 value: document.querySelector(DOMstrings.inputValue).value
             }
         }, 
+        // obj - newItem obj from step 2
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
+                element = DOMstrings.expenseContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+            
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+        // To clear input field after entering a value
+        clearFields: function() {
+            var fields, fieldsArr;
+            // querySelector outputs a list not an array | List dont have methods like array
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            // Tricking slice toconvert list to array
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+
+            // Set focus on first element of array | [0] is Add description
+            fieldsArr[0].focus();
+        },
+
         // Making DOM strings public for other modules
         getDOMstrings: function() {
             return DOMstrings;
@@ -113,10 +151,16 @@ var controller = (function(budgetCtrl, UICtrl) {
     var input, newItem;
     // 1. Get the field input data
     input = UICtrl.getInput();
+
     // 2. Add the item to the budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-    // 3. Add the item to the UI
 
+    // 3. Add the item to the UI
+    UIController.addListItem(newItem, input.type);
+
+    // 4. Clear the fields
+    UIController.clearFields();
+    
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
